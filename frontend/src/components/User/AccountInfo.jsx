dc8fc7ed971d6c8be40723b3427e51054a39ac36
ducnm168 <ductiny2003@gmail.com>
2025-06-25 16:39:12 +0700

    Honda

--- frontend/src/components/User/AccountInfo.jsx
+++ frontend/src/components/User/AccountInfo.jsx
@@ -41,7 +41,7 @@ const AccountInfo = () => {
                 <nav className="space-y-4">
                     <div className='flex items-center'>
                         <svg className='pl-1 max-w-6' fill="#57b0ff" viewBox="0 0 48 48" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" stroke="#57b0ff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><title></title><path d="M24,21A10,10,0,1,1,34,11,10,10,0,0,1,24,21ZM24,5a6,6,0,1,0,6,6A6,6,0,0,0,24,5Z"></path><path d="M42,47H6a2,2,0,0,1-2-2V39A16,16,0,0,1,20,23h8A16,16,0,0,1,44,39v6A2,2,0,0,1,42,47ZM8,43H40V39A12,12,0,0,0,28,27H20A12,12,0,0,0,8,39Z"></path></g></svg>
-                        <div className="pl-3 text-sm font-bold">Tài khoản của tôi</div>
+                        <div className="pl-3 text-sm font-bold hover:text-red-500">Tài khoản của tôi</div>
                     </div>
                     <ul className="ml-2 text-sm space-y-2">
                         <li className="">Thông tin tài khoản</li>
