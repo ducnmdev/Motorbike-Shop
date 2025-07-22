c1814736be94e571a3cfc87060739ad2af205d99
ducnm168 <ductiny2003@gmail.com>
2025-07-23 02:57:24 +0700

    fix to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
    extended: true
 }));
 app.use(cors({
-   origin: 'https://kusz.vercel.app/',
+   origin: 'https://kusz.vercel.app',
    credentials: true, // cho phép gửi cookie
 }));
 app.use(cookieParser());
